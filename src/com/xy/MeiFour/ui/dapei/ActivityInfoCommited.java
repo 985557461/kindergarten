059614701c617b61782aed5ecdff1ffdae0a992c
xiaoyu <985557461@qq.com>
2016-04-09 10:44:35 +0800

    智能搭配界面 照片上传 选择体型 上交成功  等界面搭建完毕

---
+++ src/com/xy/MeiFour/ui/dapei/ActivityInfoCommited.java
@@ -0,0 +1,45 @@
+package com.xy.MeiFour.ui.dapei;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import com.xy.MeiFour.R;
+import com.xy.MeiFour.ui.common.ActivityBaseNoSliding;
+
+/**
+ * Created by xiaoyu on 2016/4/9.
+ */
+public class ActivityInfoCommited extends ActivityBaseNoSliding implements View.OnClickListener {
+
+    public static void open(Activity activity) {
+        Intent intent = new Intent(activity, ActivityInfoCommited.class);
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_commited);
+    }
+
+    @Override
+    protected void getViews() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void setListeners() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
